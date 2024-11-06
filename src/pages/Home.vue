4c86a916d6162bba9ad5debd20f8a1ea1d96796b
Yunus Emre Bulut <yunusemrebulut123@gmail.com>
2024-11-07 00:12:03 +0300

    Home component updated for content

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -5,7 +5,7 @@
   <!-- TODO: Write a Wrapper component and use it all pages -->
   <div class="lg:container mx-auto">
     <div class="h-screen" >
-      [ Some umono information and github link etc. ]
+      Umono is an open source content management system. You can view it on <a href="https://github.com/umono-cms/umono" class="text-blue-400" target="_blank">github page</a> for more details.
     </div>
   </div>
 </template>
